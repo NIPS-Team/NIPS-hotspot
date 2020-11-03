a2d4210c91fba13c35fb2ea022570468c16c32f3
dknysh <d.knysh@nips.ru>
2020-11-03 13:12:31 +0700

    Added Copyrights.

--- src/resultsdisassemblypage.cpp
+++ src/resultsdisassemblypage.cpp
@@ -1,3 +1,25 @@
+/*
+    resultsdisassemblypage.cpp
+
+    This file is part of Hotspot, the Qt GUI for performance analysis.
+
+    Copyright (C) 2020
+    Author: Darya Knysh <d.knysh@nips.ru>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 #include "resultsdisassemblypage.h"
 #include "ui_resultsdisassemblypage.h"
 
